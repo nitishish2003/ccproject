5287ae3b1a714d188090d74345d137eecbfc0717
nitishish2003 <168121220+nitishish2003@users.noreply.github.com>
2024-04-26 10:38:26 +0530

    add.py

---
+++ add.py
@@ -0,0 +1,13 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Input two numbers from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the numbers
+result = add_numbers(num1, num2)
+
+# Display the result
+print("
